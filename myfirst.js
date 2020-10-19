c65fa853aa1d8aefd258f113c202a895a5c1a3c0
Markus <markusteiner@gmail.com>
2020-10-19 22:05:56 +0200

    test url object

--- myfirst.js
+++ myfirst.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 
+//create a server object:
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
+  res.write(req.url);
   res.end();
 }).listen(8080);
