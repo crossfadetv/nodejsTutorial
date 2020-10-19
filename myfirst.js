44bc6893dbaa8f93aa8d3cca2fd56f9ef7d40e3d
Markus <markusteiner@gmail.com>
2020-10-19 22:29:33 +0200

    add two sample html files

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,10 @@
-var http = require('http');
-var dt = require('./myfirstmodule');
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url);
-  res.end();
-}).listen(8080);
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); //returns 'february'
